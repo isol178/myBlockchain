9e8ff7c2fdaf2a1a64dbd60dd3c0da4bbf422b7f
Haruki Inaba <haruki.inb221@gmail.com>
2020-08-26 17:24:53 +0900

    ブロックのハッシュを求める　関数等の追加と型調整

--- blockchain.go
+++ blockchain.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
@@ -9,12 +11,12 @@ import (
 
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	timestamp    int64
 	transantions []string
 }
 
-func NewBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	b := new(Block) //new()はポインタを生成する
 	b.timestamp = time.Now().UnixNano()
 	b.nonce = nonce
@@ -25,8 +27,29 @@ func NewBlock(nonce int, previousHash string) *Block {
 func (b *Block) Print() {
 	fmt.Printf("timestamp        %d\n", b.timestamp)
 	fmt.Printf("nonce            %d\n", b.nonce)
-	fmt.Printf("previous_hash    %s\n", b.previousHash)
+	fmt.Printf("previous_hash    %x\n", b.previousHash)
 	fmt.Printf("transactions     %s\n", b.transantions)
+}
+
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	//fmt.Println(string(m))
+	return sha256.Sum256([]byte(m))
+}
+
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		//Marshalを使う時は大文字で始まっていないと認識されないが、ネットワーク越しでは小文字で来るので表記の調整
+		Timestamp    int64    `json:"timestamp"`
+		Nonce        int      `json:"nonce"`
+		PreviousHash [32]byte `json:"previous_hash"`
+		Transactions []string `json:"transactions"`
+	}{
+		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transantions,
+	})
 }
 
 type Blockchain struct {
@@ -35,15 +58,20 @@ type Blockchain struct {
 }
 
 func NewBlockchain() *Blockchain {
+	b := &Block{}
 	bc := new(Blockchain)
-	bc.CreateBlock(0, "init hash")
+	bc.CreateBlock(0, b.Hash())
 	return bc
 }
 
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
+}
+
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
 }
 
 func (bc *Blockchain) Print() {
@@ -61,8 +89,12 @@ func init() {
 func main() {
 	blockChain := NewBlockchain()
 	blockChain.Print()
-	blockChain.CreateBlock(5, "hash 1")
+
+	previousHash := blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(5, previousHash)
 	blockChain.Print()
-	blockChain.CreateBlock(2, "hash 2")
+
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2, previousHash)
 	blockChain.Print()
 }

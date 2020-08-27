3518dfe4e16711925f5f7c728efd763dd3ea5c6f
Haruki Inaba <isol.dev178@gmail.com>
2020-08-27 14:43:29 +0900

    トランザクションの署名

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ func init() {
 }
 
 func main() {
-	fmt.Println("start")
 	w := wallet.NewWallet()
 	fmt.Println(w.PrivateKeyStr())
 	fmt.Println(w.PublicKeyStr())
 	fmt.Println(w.BlockchainAddress())
+
+	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Printf("signature %s\n", t.GenerateSignature())
 }
